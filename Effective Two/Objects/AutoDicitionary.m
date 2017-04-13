de4b97b795fed0ea8222dede5948bee83e6e6542
Rahul Ranjan <rahul.rrixe@gmail.com>
2017-04-13 14:21:42 +0530

    update the folder and add auto dictionary

---
+++ Effective Two/Objects/AutoDicitionary.m
@@ -0,0 +1,54 @@
+//
+//  AutoDicitionary.m
+//  Effective Two
+//
+//  Created by Rahul Ranjan on 4/13/17.
+//  Copyright © 2017 Rahul Ranjan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+@interface AutoDictionary : NSObject
+@property (nonatomic, strong) NSString *string;
+@property (nonatomic, strong) NSNumber *number;
+@property (nonatomic, strong) NSDate *date;
+@property (nonatomic, strong) id opaqueObject;
+
+@property (nonatomic, strong) NSMutableDictionary *backingStore;
+
+id autoDictionaryGetter(id self, SEL _cmd);
+void autoDictionarySetter(id self, SEL _cmd, id value);
+
+@end
+
+@implementation AutoDictionary
+
+@dynamic string, number, date, opaqueObject;
+
+-(id)init {
+    if ((self = [super init])) {
+        _backingStore = [NSMutableDictionary new];
+    }
+    return self;
+}
+
++(BOOL)resolveInstanceMethod:(SEL)selector {
+    NSString *selectorString = NSStringFromSelector(selector);
+    
+    if ([selectorString hasPrefix:@"set"]) {
+        class_addMethod(self,
+                        selector,
+                        (IMP)autoDictionarySetter,
+                        "v@:@");
+    } else {
+        class_addMethod(self,
+                        selector,
+                        (IMP)autoDictionaryGetter,
+                        "@@:");
+    }
+    
+    return YES;
+}
+
+@end
